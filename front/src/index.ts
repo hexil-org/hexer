25b2922fdca685ce29a58388ddac21e621c2d8cd
Stef Gijsberts <stef@stef.link>
2022-08-18 16:15:33 +0200

    Change front to use webpack
    And update the instructions accordingly.

--- front/src/index.ts
+++ front/src/index.ts
@@ -11,9 +11,12 @@ function once(target: EventTarget, name: string) {
 }
 
 async function main() {
+    console.log("Trying to connect to the backend..");
+
     const socket = new WebSocket("ws://localhost:8080");
 
     await once(socket, "open");
+    console.log("Connected!");
 
     socket.send("ping");
     console.log("Sent ping");
