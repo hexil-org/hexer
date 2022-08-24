8882269264a70f29d77a120e732fcd08daa5ef6b
Stef Gijsberts <stef@stef.link>
2022-08-24 16:10:57 +0200

    Go back to a minimal working ping-pong server
    But now in actix.

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,5 +1,41 @@
+use actix::{Actor, StreamHandler};
+use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
 use std::env;
 
-use actix::{Actor, StreamHandler};
-use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
-use actix_web_actors::ws;
+struct ClientManager;
+
+impl Actor for ClientManager {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ClientManager {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(msg)) => {
+                if msg == "ping" {
+                    ctx.text("pong")
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+#[get("/")]
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    ws::start(ClientManager, &req, stream)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:9450".to_string());
+
+    HttpServer::new(|| App::new().service(index))
+        .bind(addr)?
+        .run()
+        .await
+}
