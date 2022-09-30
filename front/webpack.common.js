7b05d6aa28178fe213fb1ea19131ca45a01a7c62
marijnvanwezel <marijnvanwezel@gmail.com>
2022-09-30 20:12:11 +0200

    Move back to Webpack for compilation

---
+++ front/webpack.common.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.ts",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                include: path.resolve(__dirname, "src/"),
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                include: path.resolve(__dirname, "src/"),
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+    resolve: {
+        extensions: [".ts", ".js", ".json"],
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+};
