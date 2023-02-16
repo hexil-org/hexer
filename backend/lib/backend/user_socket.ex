3eff3edf2726b6cb578c73a3d0df5f1f3fc9bcb1
Stef Gijsberts <stef@stef.link>
2023-02-16 15:42:23 +0100

    Add stub for user joining

--- backend/lib/backend/user_socket.ex
+++ backend/lib/backend/user_socket.ex
@@ -2,6 +2,13 @@ defmodule Backend.UserSocket do
   use Riverside, otp_app: :backend
 
   @impl Riverside
+  def handle_message(%{"t" => "join", "game" => game_name}, session, state) do
+    # TODO: implement
+    deliver_me(%{"t" => "joined", "game" => game_name})
+    {:ok, session, state}
+  end
+
+  @impl Riverside
   def handle_message(msg, session, state) do
     deliver_me(msg)
     {:ok, session, state}
