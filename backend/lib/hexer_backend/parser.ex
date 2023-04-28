1ca1e5cc413542f6a8fa1991138029bbb0d894a2
Stef Gijsberts <stef@stef.link>
2023-04-29 00:02:20 +0200

    Add a small piece of the parser

--- backend/lib/hexer_backend/parser.ex
+++ backend/lib/hexer_backend/parser.ex
@@ -1,4 +1,44 @@
 defmodule HexerBackend.Parser do
-  def action(str) do
+  import NimbleParsec
+
+  die_value = integer(min: 1, max: 6)
+
+  roll_value =
+    ignore(string("("))
+    |> concat(die_value)
+    |> ignore(string("+"))
+    |> concat(die_value)
+    |> ignore(string(")"))
+    |> tag(:what)
+
+  roll =
+    string("R")
+    |> replace(:roll)
+    |> unwrap_and_tag(:verb)
+    |> concat(roll_value)
+
+  action = roll
+
+  defparsecp(:parsec_action, action |> eos())
+
+  def parse_action(str) do
+    {:ok, result, _, _, _, _} = parsec_action(str)
+    {:ok, to_map_deep(result)}
+  end
+
+  defp to_map_deep([{k, v} | t]) do
+    Map.put_new(to_map_deep(t), k, to_map_deep(v))
+  end
+
+  defp to_map_deep([]) do
+    %{}
+  end
+
+  defp to_map_deep(l) when is_list(l) do
+    List.to_tuple(l)
+  end
+
+  defp to_map_deep(x) do
+    x
   end
 end
--- backend/test/hexer_backend/parser_test.exs
+++ backend/test/hexer_backend/parser_test.exs
@@ -3,18 +3,18 @@ defmodule HexerBackend.ParserTest do
   alias HexerBackend.Parser
 
   test "parses 'Roll 2+3' action" do
-    assert Parser.action("R(2+3)") == %{verb: :roll, what: {2, 3}}
+    assert Parser.parse_action("R(2+3)") == {:ok, %{verb: :roll, what: {2, 3}}}
   end
 
   test "parses 'Steal' action" do
-    assert Parser.action("SO2") == %{verb: :steal, what: :ore, from: 2}
+    assert Parser.parse_action("SO2") == {:ok, %{verb: :steal, what: :ore, from: 2}}
   end
 
   test "parses 'Steal' action with unknown resource" do
-    assert Parser.action("S?1") == %{verb: :steal, what: :unknown, from: 1}
+    assert Parser.parse_action("S?1") == {:ok, %{verb: :steal, what: :unknown, from: 1}}
   end
 
   test "parses 'Abandon' action" do
-    assert Parser.action("2A(O2)") == %{who: 2, verb: :abandon, what: %{ore: 2}}
+    assert Parser.parse_action("2A(O2)") == {:ok, %{who: 2, verb: :abandon, what: %{ore: 2}}}
   end
 end
