a4e3eefb5a63cf07a0534bb593195704414ae5dc
Stef Gijsberts <stef@stef.link>
2023-04-28 22:15:19 +0200

    Add test for three more parser cases

--- backend/test/hexer_backend/parser_test.exs
+++ backend/test/hexer_backend/parser_test.exs
@@ -3,6 +3,18 @@ defmodule HexerBackend.ParserTest do
   alias HexerBackend.Parser
 
   test "parses 'Roll 2+3' action" do
-    assert Parser.action("R(2+3)") == %{type: :roll, what: {2, 3}}
+    assert Parser.action("R(2+3)") == %{verb: :roll, what: {2, 3}}
+  end
+
+  test "parses 'Steal' action" do
+    assert Parser.action("SO2") == %{verb: :steal, what: :ore, from: 2}
+  end
+
+  test "parses 'Steal' action with unknown resource" do
+    assert Parser.action("S?1") == %{verb: :steal, what: :unknown, from: 1}
+  end
+
+  test "parses 'Abandon' action" do
+    assert Parser.action("2A(O2)") == %{who: 2, verb: :abandon, what: %{ore: 2}}
   end
 end
